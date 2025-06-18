71f0c5cf7d5c19b9cfce02355e2671450775409e
Islam <islam@inaya.edu.sa>
2025-06-18 15:49:26 +0300

    installing primereact

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import {BrowserRouter, Routes, Route, Navigate} from "react-router";
 import AppRouter from "@/routes/AppRouter.jsx";
+import { PrimeReactProvider, PrimeReactContext } from 'primereact/api';
+import "primereact/resources/themes/lara-dark-purple/theme.css";
 
-function App() {
+function App({ pageProps }) {
     return (
-        <BrowserRouter>
-            <AppRouter/>
-        </BrowserRouter>
+        <PrimeReactProvider>
+            <BrowserRouter>
+                <AppRouter {...pageProps}/>
+            </BrowserRouter>
+        </PrimeReactProvider>
     );
 }
 
